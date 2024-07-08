c78cca9c6f00514d3b8b204568c4f292fd79b42e
mayenga <mayenganicholaus66@gmail.com>
2024-07-08 17:34:57 +0300

    Commited again 08/07/2024 17:34

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,7 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>smartclic | Login</title>
         <!-- plugins:css -->
         <link rel="stylesheet" href="{{ asset('admn/vendors/simple-line-icons/css/simple-line-icons.css') }}">
