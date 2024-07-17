bae875599eaee69412d1594b3be55f5b369beef4
mayenga <mayenganicholaus66@gmail.com>
2024-07-17 22:24:06 +0300

    Committed 17/07/2024 22:24

--- resources/views/welcomeMailTMP.blade.php
+++ resources/views/welcomeMailTMP.blade.php
@@ -1,24 +1,15 @@
-Dear {{ $name }},
+Hello,
 
-On behalf of the organizers, we are thankful to you for registering for the Nanenane Agricultural International Expo 2024!
+Thank you for registering through the Nanenane website.
 
-We are thrilled to have you join us for this prestigious event dedicated to showcasing the latest innovations, technologies, and developments in agriculture. Your participation is crucial in contributing to the success of this expo and in shaping the future of agriculture globally. 
+To complete the registration process for exhibitors, please make the payment for your exhibition booth on or before July 20, 2024. The payment control number is 997460011194. The arrangement of the booths will consider the type of stakeholders, the services provided, and the technologies showcased.
 
-Event Details:
+Please send the payment receipt to the email address nanenane@kilimo.go.tz. For further inquiries, please call 0712310648 or 0658860870.
 
-Date: 1st August 2024 To 8th August 2024
-Location: Dodoma Tanzania
-
-What to Expect:
-
-Networking Opportunities: Connect with industry leaders, innovators, and experts.
-Exhibits: Explore cutting-edge technologies and advancements in agriculture.
-Workshops and Seminars: Gain insights from keynote speakers and participate in informative sessions.
-Please stay tuned for further updates and detailed information regarding the schedule, agenda, and special events lined up for the expo.
-
-Once again, thank you for registering. We look forward to seeing you at the Nanenane Agricultural International Expo 2024!
+Thank you for your cooperation.
 
 Best regards,
 
-Nanenane Agricultural International Expo 2024
-        
+Ministry of Agriculture,
+Tanzania
+
